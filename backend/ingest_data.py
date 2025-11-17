89d931cfea4791b2cb1f941581b95b6676fd059f
riya chandra <riyachandra9119@gmail.com>
2025-11-17 22:48:09 +0530

    added setups

---
+++ backend/ingest_data.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenvfrom 
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.documents import Document
+
