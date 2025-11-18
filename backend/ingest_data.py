ad32f247f25dc841049b6306f788a0beb8945d90
riya chandra <riyachandra9119@gmail.com>
2025-11-18 13:20:40 +0530

    added the huggingface and vector db on dummy data

--- backend/ingest_data.py
+++ backend/ingest_data.py
@@ -1,9 +1,30 @@
 import os
-from dotenv import load_dotenvfrom 
+from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings 
 from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import gdown
 
 
-# system
+load_dotenv()
+CHROMA_DB_PATH=os.getenv("CHROMA_DB_PATH","db")
+
+
+# #load documents
+documents=[
+    Document(page_content="Meeting notes:Dicuss project X deliverables."),
+    Document(page_content="Reminder: submit report by friday."),
+    Document(page_content="upcoming event:tech confernce next wednesday.")
+] 
+
+# embeddings 
+# beside teh chromaDB we can also stoe it to the pincode ..where we can visulaize the data but in chroma db we cant'
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2") # ADD KIYA
+
+text_splitter=RecursiveCharacterTextSplitter(chunk_size=200,chunk_overlap=50)
+docs=text_splitter.split_documents(documents)
+
+vector_db = Chroma.from_documents(docs, embedding=embeddings, persist_directory=CHROMA_DB_PATH)
+print("documents succesfully indexed!")
---
+++ backend/model.py
@@ -0,0 +1,1 @@
+# intialize teh llm 
